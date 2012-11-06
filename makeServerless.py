f361d6c8a8bd93bbbc68017c01b1bb2e4b1bdee0
Oscar <oscar_villarreal-FIR@mckinsey.com>
2012-11-06 18:55:32 -0500

    reading files from template directory and concatinating them into final index file

--- makeServerless.py
+++ makeServerless.py
@@ -3,8 +3,14 @@ import inspect, os, errno, markup, sys
 templates = 'app/views/'
 index = 'app/index2.html'
 
-f = open("finale.html","w")
+final = open('final.html','w')
+
 for i in os.listdir(templates):
+	filename = 'app/views/'+i
+	f = open(filename, "r").read()
+	final.write(f)
+
+final.close()
 	
 
 
